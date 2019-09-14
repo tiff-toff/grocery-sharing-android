c26645a4e96ca3467bd2fcbd2b20a48e574d3959
Kevin Yang <hbyang2000@gmail.com>
2019-09-14 19:44:49 -0400

    request functions

--- server.py
+++ server.py
@@ -25,7 +25,7 @@ def test():
 			cursor.execute(sql,(username,password))
 		connection.commit()
 		with connection.cursor() as cursor:
-			sql =  "SELECT * from User;"
+			sql =  "SELECT Username,Password from User;"
 			cursor.execute(sql)
 			result = cursor.fetchone()
 	finally:
@@ -60,13 +60,13 @@ def createacc():
 				connection.close()
 				return "User already exists"
 		with connection.cursor() as cursor:
-			sql = "INSERT INTO User (Name, Username, Password, StreetAddress1, StreetAddress2, City, State, Zipcode, PhoneNumber) VALUES (%s, %s, %s,%s, %s,%s, %s,%s, %s)"
+			sql = "INSERT INTO User (Name, Username, Password, StreetAddress1, StreetAddress2, City, State, Zipcode, PhoneNumber) VALUES (%s, %s, %s,%s, %s,%s, %s,%s, %s);"
 			cursor.execute(sql,(name, username, password, street_address_1, street_address_2, city, state, zip_code, phone_number))
 		connection.commit()
 	finally:
 		connection.close()
 
-@app.route('/login')
+@app.route('/login', methods = ["GET"])
 def login():
 	username = request.args["username"]
 	password = request.args["password"]
@@ -74,7 +74,7 @@ def login():
 	result = 0
 	try:
 		with connection.cursor() as cursor:
-			sql = "SELECT * FROM User WHERE Username = %s AND Password = %s"
+			sql = "SELECT * FROM User WHERE Username = %s AND Password = %s;"
 			cursor.execute(sql,(username,password))
 			result = cursor.fetchone()
 	except:
@@ -84,5 +84,108 @@ def login():
 	else:
 		return "False"
 
+@app.route('/createrequest', methods = ["POST"])
+def createrequest():
+	#Active: 0-completed, 1-accepted, 3-open
+	#Type: 0 - Trip, 1 - Run
+	username = request.form["username"]
+	store = request.form["store"]
+	storeaddress = request.form["storeaddress"]
+	items = request.form["items"]
+	active = 3
+	request_type = int(request.form['type'])
+	connection = connect()
+	try:
+		with connection.cursor() as cursor:
+			sql = "INSERT INTO TripRequest (User, Store, StoreAddress Items, Active, Type) VALUES (%s,%s,%s,%s,%i,%i);"
+			cursor.execute(sql,(username,store,storeaddress,items,active,request_type))
+		connection.commit()
+	finally:
+		connection.close()
+	return "True"
+
+@app.route('/gettrips',methods = ["GET"])
+def gettrips():
+	connection = connect()
+	try:
+		with connection.cursor() as cursor:
+			sql = "SELECT (User, Store, StoreAddress, Items, TripNumber) FROM TripRequest WHERE Active = 3 AND Type = 0;"
+			cursor.execute(sql)
+			results = cursor.fetchall()
+	finally:
+		connection.close()
+	ret = ""
+	for result in results:
+		for i in result:
+			ret += str(result[i]) + ","
+		ret += ";"
+	return ret
+
+@app.route('/getruns',methods = ["GET"])
+def getruns():
+	connection = connect()
+	try:
+		with connection.cursor() as cursor:
+			sql = "SELECT (User, Store, StoreAddress, Items, TripNumber) FROM TripRequest WHERE Active = 3 AND Type = 1;"
+			cursor.execute(sql)
+			results = cursor.fetchall()
+	finally:
+		connection.close()
+	ret = ""
+	for result in results:
+		for i in result:
+			ret += str(result[i]) + ","
+		ret += ";"
+	return ret
+
+@app.route('/acceptrequests',methods = ["GET"])
+def acceptrequests():
+	acceptor = request.args["username"]
+	tripnumber = request.args["tripnumber"]
+	connection = connect()
+	try:
+		with connection.cursor() as cursor:
+			sql = "SELECT * FROM TripRequest WHERE TripNumber = %s"
+			sql = "UPDATE TripRequest SET Active = 1 WHERE TripNumber = %s"
+			cursor.execute(sql,(tripnumber))
+			result = cursor.fetchone()
+		connection.commit()
+		with connection.cursor() as cursor:
+			sql = "INSERT INTO Accepted (Requester, Acceptor, TripNumber) VALUES (%s, %s, %s)"
+			cursor.execute(sql,(username, result["Name"], tripnumber))
+			result = cursor.fetchone()
+		connection.commit()
+	finally:
+		connection.close()
+	return "True"
+@app.route('/getuser',methods = ["GET"])
+def getuser():
+	username = request.args["username"]
+	connection = connect()
+	result = 0
+	try:
+		with connection.cursor() as cursor:
+			sql = "SELECT * FROM User WHERE Username = %s"
+			cursor.execute(sql,(username))
+			result = cursor.fetchone()
+	except:
+		connection.close()
+	info = result["Name"] + "," + result["PhoneNumber"] + "," + result["City"]
+ 	return info
+
+@app.route('/getrequest', methods = ["GET"])
+def getrequest():
+	tripnumber = request.args["tripnumber"]
+	connection = connect()
+	result = 0
+	try:
+		with connection.cursor() as cursor:
+			sql = "SELECT * FROM TripRequest WHERE TripNumber = %s"
+			cursor.execute(sql,(tripnumber))
+			result = cursor.fetchone()
+	except:
+		connection.close()
+	info = result["Store"] + "," + result["StoreAddress"] + "," + result["Items"]
+ 	return info
 if __name__ == "__main__":
 	app.run(host = "0.0.0.0", port = 80)
